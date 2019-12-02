5bc50143e5139bbe5d3715ced18d79b34327e8f2
LoganNguyen <54257351+hlong24399@users.noreply.github.com>
2019-12-02 01:47:45 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,20 +10,23 @@ using namespace std;
 int main() {
 	sf::ContextSettings sets;
 	sets.antialiasingLevel = 8; 
-	sf::RenderWindow win(sf::VideoMode(800, 566), "Graph", sf::Style::Default, sets);
+	sf::RenderWindow win(sf::VideoMode(800, 600), "Graph", sf::Style::Default, sets);
+	win.setPosition(sf::Vector2i(300, 300));
 	win.setFramerateLimit(60);
 	Graph g;
-	g.ShortestPath(5,1);
 	while (win.isOpen()) {
 		sf::Event event;
 		while (win.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) win.close();
 		}
+		
 		win.clear(sf::Color::White);
 		for (auto i : g.getDrawVertices()) win.draw(i);
 		win.draw(g.getDrawEdges());
 		win.display();
+		g.options_list(); //Ask for the selection.
 	}
+	cout << " Done ";
 
 
 
